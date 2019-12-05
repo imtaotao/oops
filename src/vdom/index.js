67630398de1a7c35b67c361970d09326fe08b1d7
imtaotao <dwchentao@163.com>
2019-12-05 11:03:46 +0800

    feat: add code

--- src/vdom/index.js
+++ src/vdom/index.js
@@ -1,3 +1,34 @@
-export default function h (tag, props, ...children) {
-  return { tag, props, children }
+import vnode from './vnode.js'
+import * as is from './is.js'
+
+function addNS (data, children, sel) {
+  data.ns ='http://www.w3.org/2000/svg'
+  if (sel !== 'foreignObject' && is.isDef(children)) {
+    for (let i = 0; i < children.length; i++) {
+      const childData = children[i].data
+      if (is.isDef(childData)) {
+        addNS(childData, children[i].children, children[i].sel)
+      }
+    }
+  }
+}
+
+export default function h (sel, props, ...children) {
+  let data = {}
+  if (props !== null) data = props
+
+  if (children.length > 0) {
+    for (let i = 0; i < children.length; i++) {
+      if (is.primitive(children[i])) {
+        children[i] = vnode(undefined, undefined, undefined, children[i], undefined)
+      }
+    }
+  }
+
+  if (typeof sel === 'string' && sel.startsWith('svg') &&
+      (sel.length === 3 || sel[3] === '.' || sel[3] === '#')) {
+    addNS(data, children, sel)
+  }
+
+  return vnode(sel, data, children, undefined, undefined)
 }
